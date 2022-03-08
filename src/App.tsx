03ff4efb8ff6904578bf987a5075ba21ec31e95b
ericmuh <muhwezi1000@gmail.com>
2022-03-08 17:44:19 +0300

    memory-game

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,19 @@
 import React, { useEffect, useReducer } from "react";
 import "./App.css";
-
+import { cardArray } from "./variables";
+import blank from "./images/blank.png"
 function App() {
-  const [choice, setChoice] = React.useState("");
-  const [result, setResult] = React.useState("");
-
-  const ResultReducer = (_computerChoice: any, action: any) => {
-    switch (action.type) {
-      case 1:
-        return "rock";
-      case 2:
-        return "paper";
-      case 3:
-        return "scissors";
-      default:
-        return "";
-
-    }
-  };
-
-
-  const checkResults = () => {
-    if (computerChoice === choice) {
-      setResult("its a draw!");
-    }
-    if (computerChoice === "rock" && choice === "paper") {
-      setResult("you win!");
-    }
-    if (computerChoice === "rock" && choice === "scissors") {
-      setResult("you lost!");
-    }
-    if (computerChoice === "paper" && choice === "scissors") {
-      setResult("you win!");
-    }
-    if (computerChoice === "paper" && choice === "rock") {
-      setResult("you lose!");
-    }
-    if (computerChoice === "scissors" && choice === "rock") {
-      setResult("you win!");
-    }
-    if (computerChoice === "scissors" && choice === "paper") {
-      setResult("you lose!");
-    }
-  };
-  const initialState: any = "";
-  const [computerChoice, dispatch] = useReducer(ResultReducer, initialState);
-
-  const handleClick = (e: any) => {
-    dispatch({ type: Math.floor(Math.random() * 3) + 1 });
-    setChoice(e.target.id);
-    checkResults()
-    console.log(e.target.id);
-  };
-
+  cardArray.sort(() => 0.5 - Math.random());
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
   return (
     <div className="App">
-      <h2>Your Choice: {choice}</h2>
-      <h2>Computer Choice: {computerChoice}</h2>
-
-      <button id="rock" onClick={handleClick}>
-        Rock
-      </button>
-      <button id="paper" onClick={handleClick}>
-        Paper
-      </button>
-      <button id="scissors" onClick={handleClick}>
-        Scissors
-      </button>
-      <h1>{result} : {computerChoice}:{choice}</h1>
+      <div className="grid">
+        {cardArray.map((card, key) => (
+          <img key={key} src={blank} alt={card.name} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/variables.ts
@@ -0,0 +1,50 @@
+export const cardArray = [
+  {
+    name: "fries",
+    img: require("./images/fries.png"),
+  },
+  {
+    name: "cheeseburger",
+    img: require("./images/cheeseburger.png"),
+  },
+  {
+    name: "ice-cream",
+    img: require("./images/ice-cream.png"),
+  },
+  {
+    name: "pizza",
+    img: require("./images/pizza.png"),
+  },
+  {
+    name: "milkshake",
+    img: require("./images/milkshake.png"),
+  },
+  {
+    name: "hotdog",
+    img: require("./images/hotdog.png"),
+  },
+  {
+    name: "fries",
+    img: require("./images/fries.png"),
+  },
+  {
+    name: "cheeseburger",
+    img: require("./images/cheeseburger.png"),
+  },
+  {
+    name: "ice-cream",
+    img: require("./images/ice-cream.png"),
+  },
+  {
+    name: "pizza",
+    img: require("./images/pizza.png"),
+  },
+  {
+    name: "milkshake",
+    img: require("./images/milkshake.png"),
+  },
+  {
+    name: "hotdog",
+    img: require("./images/hotdog.png"),
+  },
+];
